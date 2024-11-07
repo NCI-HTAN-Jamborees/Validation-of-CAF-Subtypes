0e8593aa6eb9651a29fd0ba392318bb95c0cc629
Nathan W <38703762+wong-nw@users.noreply.github.com>
2024-11-07 13:39:02 -0500

    Add files via upload

---
+++ ConvertHTAN_SeuratSlide.R
@@ -0,0 +1,13 @@
+
+ConvertSeuratToSpatial = function (spatialSeuratHTAN){
+  spatialObject=CreateSeuratObject(counts=spatialSeuratHTAN@assays$RNA@counts,assay="Spatial")
+  coord.df =data.frame(x= -Embeddings(spatialSeuratHTAN@reductions$spatial)[,2], y=Embeddings(spatialSeuratHTAN@reductions$spatial)[,1])
+  spatialObject@images$image =  new(
+  Class = 'SlideSeq',
+  assay = "Spatial",
+  key = "image_",
+  coordinates = coord.df
+  )
+  spatialObject@meta.data= cbind(spatialObject@meta.data,spatialSeuratHTAN@meta.data)
+  return(spatialObject)
+}
